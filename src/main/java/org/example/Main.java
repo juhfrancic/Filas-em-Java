12df4ac7bf145e72d66215fed01ac17b5b3cb661
juhfrancic <juliafrancictostes@gmail.com>
2026-04-27 21:47:27 -0300

    Subindo codigo completo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
+import java.util.PriorityQueue;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PriorityQueue<Chamado> fila = new PriorityQueue<>();
+        fila.add(new Chamado("Caiu", 2));
+        fila.add(new Chamado("Computador não liga", 5));
+        fila.add(new Chamado("Erro no sistema", 4));
+        fila.add(new Chamado("Troca de senha", 3));
+        fila.add(new Chamado("Quebrou", 1));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while(!fila.isEmpty()){
+            Chamado  chamado = fila.poll();
+            System.out.println(chamado.descricao + " - prioridade: " + chamado.prioridade);
         }
     }
 }
