36ed5ec01c969b06eb2b6ce62237a0f5a7fa75d0
juhfrancic <juliafrancictostes@gmail.com>
2026-04-27 21:17:08 -0300

    Commit inicial

---
+++ src/main/java/org/example/Paciente.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Paciente implements Comparable<Paciente>{
+    String nome;
+    int prioridade;
+
+    public Paciente(String nome, int prioridade){
+        this.nome = nome;
+        this.prioridade = prioridade;
+    }
+
+    @Override
+    public int compareTo(Paciente outro){
+        //Garante que a maior prioridade venha primeiro
+        return outro.prioridade - this.prioridade;
+    }
+}
